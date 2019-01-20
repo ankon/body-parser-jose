3126191390d0ee15abd1b6a13056d3c7e3a29dc9
Andreas Kohn <andreas.kohn@gmail.com>
2019-01-20 16:42:56 +0100

    Add code

---
+++ lib/index.ts
@@ -0,0 +1,25 @@
+import { parse as parseContentType } from 'content-type';
+import { IncomingMessage, ServerResponse } from 'http';
+import getRawBody from 'raw-body';
+import { decode } from 'jsonwebtoken';
+
+export function jose(contentTypes = ['application/jose']) {
+    return async(req: IncomingMessage, res: ServerResponse, next: Function) => {
+        try {
+            const contentType = parseContentType(req);
+            if (contentTypes.includes(contentType.type)) {
+                const rawBody = await getRawBody(req, {
+                    length: req.headers['content-length'],
+                    limit: '1mb',
+                    encoding: contentType.parameters.charset,
+                });
+                // TODO: Support providing additional options for decoding/decrypting/verifying
+                const body = decode(rawBody, {complete: true});
+                Object.defineProperty(req, 'body', {value: body});    
+            }
+            next();
+        } catch (err) {
+            next(err);
+        }
+    };
+}
